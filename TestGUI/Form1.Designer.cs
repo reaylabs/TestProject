2e5a338e004b4a6450faf7d8e62bfd99b5bc5d37
Robert Reay <bobreay@yahoo.com>
2023-06-25 20:47:46 -0700

    Added the count to the TestGui. Renamed the copy of the build instructions.

--- TestGUI/Form1.Designer.cs
+++ TestGUI/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbCount
+            // 
+            this.tbCount.Location = new System.Drawing.Point(7, 115);
+            this.tbCount.Name = "tbCount";
+            this.tbCount.Size = new System.Drawing.Size(163, 39);
+            this.tbCount.TabIndex = 1;
+            this.tbCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbCount);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox tbCount;
     }
 }
--- TestGUI/Form1.cs
+++ TestGUI/Form1.cs
@@ -2,9 +2,17 @@ namespace TestGUI
 {
     public partial class Form1 : Form
     {
+        int count = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            tbCount.Text = count.ToString();
         }
     }
 }
